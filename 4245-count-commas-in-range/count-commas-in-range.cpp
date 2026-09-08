553946539692fa0eae8c5dec8dae1ba65f297cab
Kaustubh Keni <kaustubhkeni02@gmail.com>
2026-09-08 17:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.38%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int num = n;
-        int count = 0;
-        while(num!=0){
-            int temp = num % 10;
-            count++;
-            num = num / 10;
-        }
+        // int num = n;
+        // int count = 0;
+        // while(num!=0){
+        //     int temp = num % 10;
+        //     count++;
+        //     num = num / 10;
+        // }
 
-        if(count<4){
+        // if(count<4){
+        //     return 0;
+        // }
+
+        // return n - 1000 + 1;
+
+        if(n<1000){
             return 0;
         }
 
